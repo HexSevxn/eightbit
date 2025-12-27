9d22926c86739600f0e1519ebc68a3fcc35e5312
ItzHex <helplex105@gmail.com>
2025-12-27 08:42:11 -0500

    creation

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::fs::read_to_string;
+
+pub mod instruction;
+pub mod interpreter;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let path = args[1].clone();
+    if !path.ends_with(".e8") {
+        panic!("File is not an e8 program!");
+    };
+
+    let program = read_to_string(path).expect("Error reading file.");
+    println!("{}\n{}", program.len(), program);
+}
